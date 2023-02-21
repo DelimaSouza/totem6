915d7d19195096067e3ed06be38637ec2e810675
TheDiasz <113309142+TheDiasz@users.noreply.github.com>
2023-02-21 18:17:24 -0300

    Update horario.js

--- assets/js/horario.js
+++ assets/js/horario.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     var time = new Date().getTime();
     
        //Set the start hours and ending hours
-    if (time > start.setHours(04,30) && time < end.setHours(17,23)) {
+    if (time > start.setHours(04,30) && time < end.setHours(18,22)) {
         $('.criar2').show();
         $('.fechado').hide();
     }
