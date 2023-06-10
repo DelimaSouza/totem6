adf090bba749e5c948cefea26d35bfaf89018954
TheDiasz <113309142+TheDiasz@users.noreply.github.com>
2023-06-10 18:46:19 -0300

    Update horario.js

--- assets/js/horario.js
+++ assets/js/horario.js
@@ -1,1 +1,19 @@
+$(document).ready(function () {
 
+    var start = new Date();
+    var end = new Date();
+    var time = new Date().getTime();
+    
+       //Set the start hours and ending hours
+    if (time > start.setHours(04,30) && time < end.setHours(08,00)) {
+        $('.criar2').show();
+        $('.fechado').hide();
+    }
+    else {
+        $('.criar2').hide();
+        $('.aluno').hide();
+        $('.serie').hide();
+        $('.data').hide();
+        $('.fechado').show();
+        }
+    });
